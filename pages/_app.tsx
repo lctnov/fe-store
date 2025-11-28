b33b5c216857a786e86940668b8f52c4ea2087d1
lctnov <chauthoi1010@gmail.com>
2025-11-28 13:39:06 +0700

    update app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import { CartProvider } from "@/hooks/useCart";
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+
 import { Toaster } from "react-hot-toast";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <CartProvider>
       <Component {...pageProps} />
+
       <Toaster
         position="top-right"
         toastOptions={{
@@ -17,6 +21,18 @@ export default function App({ Component, pageProps }) {
             background: '#333',
             color: '#fff',
             fontSize: '14px'
+          },
+          success: {
+            iconTheme: {
+              primary: '#4ade80',
+              secondary: '#fff'
+            }
+          },
+          error: {
+            iconTheme: {
+              primary: '#ef4444',
+              secondary: '#fff'
+            }
           }
         }}
       />
